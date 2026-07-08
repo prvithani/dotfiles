0d691ba87df79d2e799624297e04e0c4d092f290
Prashant Vithani <prashantvithani@gmail.com>
2026-07-08 13:21:31 +0000

    chore(doom): Refactor Emacs version profiles
    Remove obsolete `igc` profiles and consolidate into a single, simpler profile for Emacs 31.

--- .config/doom/profiles.el
+++ .config/doom/profiles.el
@@ -1,9 +1,5 @@
 ;;; profiles.el -*- lexical-binding: t; -*-
 
 ((default)
- (igc (user-emacs-directory . "~/.config/emacs")
-      ("EMACS_VERSION" . "igc"))
- (31-igc (user-emacs-directory . "~/.config/emacs")
-	 ("EMACS_VERSION" . "31-igc"))
- (32-igc (user-emacs-directory . "~/.config/emacs")
-	 ("EMACS_VERSION" . "32-igc")))
+ (31 (user-emacs-directory . "~/.config/emacs")
+     ("EMACS_VERSION" . "31")))
